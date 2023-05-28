227efde388266b48c712ef5793274b5f991cbf19
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-05-28 12:27:33 +0300

    Use bin folder

--- src/install.sh
+++ src/install.sh
@@ -8,22 +8,32 @@ set -e
 
 title () { printf "\n░▒▓█ $1 \n" ; }
 subtitle () { printf "\n■► $1\n" ; }
+recreatedir () {
+    [[ -d $1 ]] && sudo rm -rf $1
+    sudo mkdir --parents $1
+}
 
 title "Installing iukbtw"
 
 BINDIR=/opt/iukbtw/bin
+USRBINDIR=/usr/bin/iukbtw
 CONFDIR=/etc/opt/iukbtw
-[[ -d $BINDIR ]] && sudo rm -rf $BINDIR
-sudo mkdir --parents $BINDIR
-[[ -d $CONFDIR ]] && sudo rm -rf $CONFDIR
-sudo mkdir --parents $CONFDIR
+recreatedir $BINDIR
+recreatedir $USRBINDIR
+recreatedir $CONFDIR
 
 # Copy scripts
 ls -1 ./scripts/ | while read filename ; do
     targetfile=$(echo $filename| cut -d. -f1)
     sudo cp ./scripts/$filename $BINDIR/$targetfile
     sudo chmod +x $BINDIR/$targetfile
+    sudo cp -s $BINDIR/$targetfile $USRBINDIR
 done
+# Add bin dir to PATH
+USRPROF=$HOME/.profile
+[[ -f $USRPROF ]]
+sed -e $(cat $HOME/.profile | grep iuk -n | cut -d: -f1)d $USRPROF
+printf "\nexport PATH=$BINDIR:\$PATH  # Add iukbtw commands to PATH\b" >> $USRPROF
 # Copy config
 sudo cp --recursive ./config/* $CONFDIR
 
@@ -31,7 +41,7 @@ sudo cp --recursive ./config/* $CONFDIR
 if [[ $1 != "nodeps" ]]; then
     # Install dependencies
     subtitle "Installing dependencies"
-    yay -S --needed - < ./deps.txt
+    yay -S --needed --noconfirm - < ./deps.txt
     # Install Python libraries
     subtitle "Installing Python libraries"
     python -m pip install arrow
@@ -47,7 +57,7 @@ if [[ $(visudo -csf ./sudoers | grep "parsed OK") = "" ]] ; then
     echo "Failed check on sudoer file"
     exit 1
 else
-    sudo cp ./sudoers /etc/sudoers.d/50-iukbtw
+    sudo cp --force ./sudoers /etc/sudoers.d/50-iukbtw
 fi
 # Add udev rules for KMonad
 # (https://github.com/kmonad/kmonad/issues/160#issuecomment-766121884)
@@ -56,13 +66,6 @@ sudo usermod -aG input $USER
 echo uinput | sudo tee /etc/modules-load.d/uinput.conf
 echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/90-uinput.rules
 
-
-# Copy scripts
-rm -rf $HOME/.scripts
-mkdir $HOME/.scripts
-ls -1 $BINDIR | while read exe; do
-    cp -s $BINDIR/$exe $HOME/.scripts/$exe
-done
 
 # Copy dotfiles
 subtitle "Copying user home skeleton"
@@ -75,4 +78,5 @@ subtitle "Done."
     -z $(groups | grep input) ||
     -z $(groups | grep uinput) ||
     -z $(groups | grep iukbtw)
-]] && echo "Please restart for group config to apply."
+]] && echo "Please login again for group config to apply."
+[[ -z $(echo $PATH | grep $USRBINDIR) ]] && echo "Please login again for PATH to apply"
