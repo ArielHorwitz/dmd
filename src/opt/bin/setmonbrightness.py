7b38650b48bfa3e08a0adfa4fc33d6d87d946dad
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-06-04 08:34:49 +0300

    Monitor brightness controls

---
+++ src/opt/bin/setmonbrightness.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+
+import subprocess
+import argparse
+
+
+def run(*command, **kwargs):
+    kwargs = dict(capture_output=True, check=True) | kwargs
+    result = subprocess.run(command, **kwargs)
+    return result.stdout.decode().strip()
+
+
+def get_device():
+    name = run("ls", "-1", "/sys/class/backlight/")
+    return f"/sys/class/backlight/{name}"
+
+
+def get_brightness(device):
+    return int(run("cat", f"{device}/brightness"))
+
+
+def get_max_brightness(device):
+    return int(run("cat", f"{device}/max_brightness"))
+
+
+def set_brightness(device, brightness):
+    run("tee", f"{device}/brightness", input=str(brightness).encode())
+
+
+def main():
+    # Parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--increase",
+        help="Increase brightness",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-d",
+        "--decrease",
+        help="Decrease brightness",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-s",
+        "--set",
+        help="Set brightness (0-10)",
+        type=int,
+    )
+    args = parser.parse_args()
+    # Collect parameters
+    device = get_device()
+    brightness = get_brightness(device)
+    max_brightness = get_max_brightness(device)
+    # Find new brightness value
+    if args.set:
+        steps = max(0, min(int(args.set), 10))
+        value = max_brightness * (2 ** (-10 + steps))
+    elif args.increase:
+        value = brightness * 2
+    elif args.decrease:
+        value = brightness / 2
+    else:
+        value = max_brightness
+    new_brightness = round(max(0, min(value, max_brightness)))
+    print(f"Setting brightness: {new_brightness}")
+    set_brightness(device, new_brightness)
+
+
+if __name__ == "__main__":
+    main()
