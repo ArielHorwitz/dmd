0686a14a3684848a06d2c5863c9e6a1d91b62aa0
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-06-28 14:18:09 +0300

    Disable firefox tabs

---
+++ src/opt/bin/disablefirefoxtabs.py
@@ -0,0 +1,33 @@
+#!/bin/python
+
+import shutil
+from pathlib import Path
+from configparser import ConfigParser
+
+
+FF_DIR = Path.home() / ".mozilla" / "firefox"
+PREF_SHEETS = (
+    'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);\n'
+)
+USERCHROME_CSS = "toolbar#TabsToolbar { display: none !important }\n"
+
+
+def main():
+    config_parser = ConfigParser()
+    config_parser.read(FF_DIR / "installs.ini")
+    profile = config_parser[config_parser.sections()[0]]['Default']
+    profile_dir = FF_DIR / profile
+    print(f"Profile dir: {profile_dir}")
+    userchrome = profile_dir / "chrome" / "userChrome.css"
+    userchrome.parent.mkdir(exist_ok=True)
+    if userchrome.is_file():
+        shutil.copy(userchrome, userchrome.with_suffix(".css.bak"))
+    userchrome.write_text(USERCHROME_CSS)
+    userprefs = profile_dir / "user.js"
+    if userprefs.is_file():
+        shutil.copy(userprefs, userprefs.with_suffix(".js.bak"))
+    userprefs.write_text(PREF_SHEETS)
+
+
+if __name__ == "__main__":
+    main()
