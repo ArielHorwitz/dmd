025754db8de5d6ab0a6bdc94c7e97f46823e3d76
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-14 15:29:31 +0300

    Fix bug with basepath and absolute filepath

--- src/opt/bin/pathparent.py
+++ src/opt/bin/pathparent.py
@@ -5,7 +5,17 @@ import argparse
 import sys
 from pathlib import Path
 
-parser = argparse.ArgumentParser()
+DESCRIPTION = """Get the parent directory of a given path.
+
+If basepath is given and filepath is relative, concatenate the paths.
+
+Use --resolve to resolve symlinks and expand the path as an absolute path.
+
+Use --confirm-exists to ensure the resulting path exists.
+"""
+
+# Parse args
+parser = argparse.ArgumentParser(description="")
 parser.add_argument("FILEPATH", help="File path")
 parser.add_argument("BASEPATH", nargs="?", help="Absolute base path")
 parser.add_argument(
@@ -18,25 +28,32 @@ parser.add_argument(
     "-e",
     "--confirm-exists",
     action="store_true",
-    help="Fail if final result does not exist",
+    help="Fail if result does not exist",
 )
 args = parser.parse_args()
-path = Path(args.FILEPATH)
-if args.BASEPATH is None:
-    base = Path("/")
-else:
+
+# Naive result
+result = Path(args.FILEPATH).parent
+
+# Add base if result is relative
+if not result.is_absolute() and args.BASEPATH is not None:
     base = Path(args.BASEPATH)
-parent = (base / path).parent
-if not parent.is_relative_to(base):
-    print(
-        f"Filepath '{path}' is not relative to basepath '{base}'",
-        file=sys.stderr,
-    )
-    quit(1)
+    result = base / result
+    if not result.is_relative_to(base):
+        print(
+            f"Filepath '{result}' is not relative to basepath '{base}'",
+            file=sys.stderr,
+        )
+        quit(1)
+
+# Resolve and expand
 if args.resolve:
-    parent = parent.resolve()
-if args.confirm_exists and not parent.exists():
-    print(f"Result does not exist: {parent}", file=sys.stderr)
+    result = result.resolve()
+
+# Confirm exists
+if args.confirm_exists and not result.exists():
+    print(f"Resulting path does not exist: {result}", file=sys.stderr)
     quit(1)
-print(parent)
+
+print(result)
 
