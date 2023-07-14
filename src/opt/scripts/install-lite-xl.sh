9c0019b783bf58563c134c7bb146a42f6c5acf59
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-14 13:09:29 +0300

    Install desktop entry and icon

--- src/opt/scripts/install-lite-xl.sh
+++ src/opt/scripts/install-lite-xl.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/bash
 
+DATADIR="$(pathparent $BASH_SOURCE $PWD -re)/data"
+[[ ! -d $DATADIR ]] && echo "Missing data directory: $DATADIR" >&2 && exit 1
+
 set -e
 
 TMPDIR="/tmp/install-lite-xl/"
@@ -8,7 +11,7 @@ rm -rf $TMPDIR
 mkdir -p $TMPDIR
 cd $TMPDIR
 
-# Lite XL v2.1.1
+# Download Lite XL v2.1.1
 echo "Downloading..."
 URL="https://github.com/lite-xl/lite-xl/releases/download/v2.1.1/lite-xl-v2.1.1-addons-linux-x86_64-portable.tar.gz"
 echo "  $URL"
@@ -23,12 +26,23 @@ echo "Copying files..."
 chmod +x lite-xl
 mkdir -p $HOME/.local/bin
 mv -f lite-xl $HOME/.local/bin/
-xdg-desktop-menu forceupdate
 
 # Copy core and addons
 rm -rf $HOME/.local/share/lite-xl
 mkdir -p $HOME/.local/share/lite-xl
 mv -f data/* $HOME/.local/share/lite-xl/
+
+# XDG menus
+DESKTOP_APPS="$HOME/.local/share/applications/"
+DESKTOP_ICONS="$HOME/.local/share/icons/hicolor/48x48/apps/"
+mkdir -p $DESKTOP_APPS
+mkdir -p $DESKTOP_ICONS
+cp -f $DATADIR/lite-xl.desktop $DESKTOP_APPS
+cp -f $DATADIR/lite-xl.png $DESKTOP_ICONS
+xdg-desktop-menu forceupdate
+
+# LSP
+set +e
 
 echo "Installing LSP..."
 mkdir -p $HOME/.config/lite-xl/
@@ -43,6 +57,7 @@ python -m pip install --break-system-packages python-lsp-server
 npm install -g bash-language-server
 npm install -g dockerfile-language-server-nodejs
 
-echo "Cleaning up..."
+
+echo "Cleaning up temporary working directory $TMPDIR"
 rm -rf $TMPDIR
 
