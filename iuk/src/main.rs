018b3ec9ccb43961e239db9aff9cf9f49cf8e1bc
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-28 07:27:24 +0300

    Setup iuk

---
+++ iuk/src/main.rs
@@ -0,0 +1,25 @@
+use anyhow::Result;
+use clap::Parser;
+use std::process::Command;
+use iuk::run;
+
+#[derive(Parser, Debug)]
+#[command(name = "iukbtw")]
+#[command(author = "https://ariel.ninja")]
+#[command(about)]
+#[command(version)]
+struct Args {
+    /// List binaries
+    #[arg(short, long)]
+    list: bool,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    if args.list {
+        run(Command::new("ls")
+            .arg("-1")
+            .arg("/opt/iukbtw/bin/"))?;
+    }
+    Ok(())
+}
