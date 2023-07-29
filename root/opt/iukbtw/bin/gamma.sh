fbad25dbd28be59fe8aa43c8f4cd0c17e7779c88
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-07-29 19:21:31 +0300

    Gamma script

---
+++ root/opt/iukbtw/bin/gamma.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+N=1
+H=0.9
+M=0.8
+L=0.7
+gamma="1"
+case $1 in
+    red       ) gamma=$N:$M:$M ;;
+    green     ) gamma=$M:$N:$M ;;
+    blue      ) gamma=$M:$M:$N ;;
+    orange    ) gamma=$N:$H:$L ;;
+    cyan      ) gamma=$L:$H:$N ;;
+    purple    ) gamma=$H:$L:$N ;;
+esac
+
+for monitor in $(listmonitors) ; do
+    xrandr --output $monitor --gamma $gamma
+done
