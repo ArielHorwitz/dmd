fba01a1020804f65f52325b3573ac4f35b928ac2
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-08-11 15:47:52 +0300

    Ignore target folder

--- root/opt/iukbtw/bin/lt.sh
+++ root/opt/iukbtw/bin/lt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 tree_args="-aC"
-tree_ignore="-I .git/ -I venv/ -I __pycache__/ -I *.egg-info/"
+tree_ignore="-I .git/ -I venv/ -I __pycache__/ -I *.egg-info/ -I target/"
 
 tree $@ $tree_args $tree_ignore | bat
