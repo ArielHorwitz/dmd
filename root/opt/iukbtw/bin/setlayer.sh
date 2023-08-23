df621851776951ba7d6a272a1409851653c51d47
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-08-23 19:16:08 +0300

    Remove layer led control

--- root/opt/iukbtw/bin/setlayer.sh
+++ root/opt/iukbtw/bin/setlayer.sh
@@ -3,10 +3,6 @@
 [[ $EUID -ne 0 ]] && echo "Must run $0 as root" >&2 && exit 1
 
 [[ -n $1 ]] && LAYER=$1 || LAYER="base"
-if [[ -z $(grep -x $LAYER < /etc/opt/iukbtw/layers) ]] ; then
-    echo Unknown layer '$LAYER' >&2
-    exit 1
-fi
 
 # Set layer state
 mkdir --parents /var/opt/iukbtw
@@ -17,19 +13,3 @@ case $LAYER in
     base ) /opt/iukbtw/bin/gamma;;
     text ) /opt/iukbtw/bin/gamma blue;;
 esac
-
-# Set keyboard LEDs
-GPROFILE="/etc/opt/iukbtw/g610profiles/$LAYER"
-echo "Setting g610 profile: $GPROFILE"
-[[ -f $GPROFILE ]] && g610-led -p $GPROFILE
-while read line ; do
-    [[ -z $line ]] && continue
-    LED_NAME=$(echo $line | cut -d' ' -f1)
-    LED="/sys/class/leds/$(ls -1 /sys/class/leds/ | grep $LED_NAME)"
-    BRIGHTNESS=$(echo $line | cut -d' ' -f2)
-    if [[ -f $LED/brightness ]]; then
-        [[ $BRIGHTNESS -lt 0 ]] && BRIGHTNESS=$(cat $LED/max_brightness)
-        printf "Switching $LED $(cat $LED/brightness) > $BRIGHTNESS"
-        echo $BRIGHTNESS | tee $LED/brightness
-    fi
-done < "/etc/opt/iukbtw/leds/$LAYER"
