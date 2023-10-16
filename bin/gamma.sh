2be9ab5fd6c1e01c45ec2d32a227ffec2b15ea90
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-10-16 18:53:51 +0300

    Adjust colors

--- bin/gamma.sh
+++ bin/gamma.sh
@@ -1,17 +1,16 @@
 #! /bin/bash
 
-N=1
-H=0.9
-M=0.75
-L=0.6
+H=1.2
+M=1
+L=0.7
 gamma="1"
 case $1 in
-    red       ) gamma=$N:$M:$M ;;
-    green     ) gamma=$M:$N:$M ;;
-    blue      ) gamma=$M:$M:$N ;;
-    orange    ) gamma=$N:$H:$L ;;
-    cyan      ) gamma=$L:$H:$N ;;
-    purple    ) gamma=$H:$L:$N ;;
+    red       ) gamma=$H:$M:$M ;;
+    green     ) gamma=$M:$H:$M ;;
+    blue      ) gamma=$M:$M:$H ;;
+    orange    ) gamma=$H:$M:$L ;;
+    cyan      ) gamma=$L:$H:$M ;;
+    purple    ) gamma=$M:$L:$H ;;
 esac
 
 for monitor in $(listmonitors) ; do
--- bin/setlayer.sh
+++ bin/setlayer.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+TEXTCOLOR="cyan"
+
 case $1 in
     base ) polybar-msg action kmd hook 0 && /usr/bin/iukbtw/gamma;;
-    text ) polybar-msg action kmd hook 1 && /usr/bin/iukbtw/gamma green;;
+    text ) polybar-msg action kmd hook 1 && /usr/bin/iukbtw/gamma $TEXTCOLOR;;
     *    ) echo "No such layer" >&2;;
 esac
