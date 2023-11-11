8913506108c7c03d738af1c102f53dca2b88d709
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-11-11 11:30:55 +0200

    Add finer volume control

--- etc/kmd/audio.hs
+++ etc/kmd/audio.hs
@@ -4,14 +4,25 @@
   _     XX    XX    XX    XX    XX    XX    @aimt @volu @aium @sksp XX    XX    XX     XX    XX    XX     XX    XX    XX    XX
   _     XX    XX    XX    XX    XX    XX    @prev @vold @next @skhd XX    _                               XX    XX    XX
   _     XX    XX    XX    XX    XX    XX    @aomt @vol0 @aoum XX    _                        XX           XX    XX    XX    XX
-  _     _     _                 @pp               _     _     _     _                  XX    XX    XX     XX    XX
+  _     _     @aud2             @pp               _     _     _     _                  XX    XX    XX     XX    XX
+)
+(deflayer audio_alt
+  _     _     _     _     _     _     _     _     _     _     _     _     _            XX    XX    XX
+  _     _     _     _     _     _     _     _     _     _     _     _     _     _      XX    XX    XX     XX    XX    XX    XX
+  _     _     _     _     _     _     _     _     @volU _     _     _     _     _      XX    XX    XX     XX    XX    XX    XX
+  _     _     _     _     _     _     _     _     @volD _     _     _     _                               XX    XX    XX
+  _     _     _     _     _     _     _     _     _     _     _     _                        XX           XX    XX    XX    XX
+  _     _     _                 _                 _     _     _     _                  XX    XX    XX     XX    XX
 )
 (defalias
+    aud2 (layer-toggle audio_alt)
     sksp (cmd-button "audio_speakers")
     skhd (cmd-button "audio_headphones")
-    volu (cmd-button "iukmessenger audio --increase 5")
-    vold (cmd-button "iukmessenger audio --decrease 5")
-    vol0 (cmd-button "iukmessenger audio 0")
+    volu (cmd-button "iukmessenger audio --increase 2")
+    vold (cmd-button "iukmessenger audio --decrease 2")
+    volU (cmd-button "iukmessenger audio --increase 10")
+    volD (cmd-button "iukmessenger audio --decrease 10")
+    vol0 (cmd-button "iukmessenger audio 10")
     aimt (cmd-button "micmute")
     aium (cmd-button "micunmute")
     aomt (cmd-button "iukmessenger audio --mute")
