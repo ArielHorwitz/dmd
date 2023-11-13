5aaa8c177d534bd03a6807522dc1c513582f912d
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2023-11-13 18:42:15 +0200

    Improve startup script

--- home/.config/i3/lembay.sh
+++ home/.config/i3/lembay.sh
@@ -1,18 +1,15 @@
+#! /bin/bash
+
 iuk log "Lembay startup"
 xrandr --output HDMI-1 --primary
 xrandr --output DP-2 --right-of HDMI-1
 xrandr --output DP-2 --rotate left
-i3-msg "workspace main; append_layout $HOME/.config/i3/main.json"
 i3-msg "workspace aux; append_layout $HOME/.config/i3/aux.json"
-i3-msg "workspace 9; append_layout $HOME/.config/i3/iuk.json"
-sleep 100ms
-alacritty --hold --title "iukdaemon" --command iukdaemon &
-alacritty --hold --title "kmonad" --command kmdrun -l debug &
-sleep 200ms
+i3-msg "workspace main; append_layout $HOME/.config/i3/main.json"
+sleep 0.5
 lite-xl &
 alacritty --title "main1" &
 alacritty --title "main2" &
 alacritty --title "aux" &
-$HOME/.config/launchers/lazygit.sh &
 iuk log "Lembay startup complete"
 
--- home/.config/i3/startup.sh
+++ home/.config/i3/startup.sh
@@ -1,3 +1,39 @@
+#! /bin/bash
+
+SLEEPTIME=0.2
+
 iuk log -r "Startup"
+
+# iuk
+alacritty --title "iukdaemon" --command iukdaemon &
+sleep $SLEEPTIME
+iukmessenger scratch --move 7
+iukmessenger scratch --show 7
+sleep $SLEEPTIME
+xdotool getactivewindow windowsize --sync 800 900
+xdotool getactivewindow windowmove --sync 25 25
+iukmessenger scratch --show 7
+
+# kmd
+alacritty --title "kmonad" --command kmdrun &
+sleep $SLEEPTIME
+iukmessenger scratch --move 7
+iukmessenger scratch --show 7
+sleep $SLEEPTIME
+xdotool getactivewindow windowsize --sync 800 900
+xdotool getactivewindow windowmove --sync 850 25
+iukmessenger scratch --show 7
+
+# lazygit
+alacritty -e lazygit &
+sleep $SLEEPTIME
+iukmessenger scratch --move 8
+iukmessenger scratch --show 8
+sleep $SLEEPTIME
+xdotool getactivewindow windowsize --sync 1800 900
+windowcenter
+iukmessenger scratch --show 8
+
+
 $HOME/.config/i3/lembay.sh
 
