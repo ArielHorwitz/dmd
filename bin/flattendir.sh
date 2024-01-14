72a774d2fc1d16ecfbd1bed2ebcb5fb9725c30d5
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-01-14 17:13:34 +0200

    Rework flattendir using spongecrab

--- bin/flattendir.sh
+++ bin/flattendir.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-if [[ $# -lt 1 ]]; then
-  echo "Usage: $0 <nested-dir> [--clean]" >&2
-  exit 1
-fi
-
 set -e
 
-echo "Flattening: $1"
-find $1 -mindepth 2 -type f -exec mv --backup=numbered -t $1 '{}' +
-echo "Flat."
+# Create cli and parse arguments
+spongecrab_args=$(
+    CLI="nested target -f force -f verbose"
+    NAME="flattendir"
+    ABOUT="Recursively copy files from directory to a flat directory."
+    spongecrab $CLI --name $NAME --about "$ABOUT" -- $@
+) || { echo $spongecrab_args; exit 1 # Print help or errors and quit
+}; eval $spongecrab_args # Evaluate results
 
-if [[ $2 = "--clean" ]] ; then
-  echo "Cleaning up..."
-  rm -f $1/*~
-  find $1 -mindepth 1 -type d -exec rm -rf '{}' +
-fi
+# Verify arguments and prepare target dir
+[[ -d $nested ]] || { tcprint "error]'$nested' is not a directory"; exit 1; }
+[[ -d $target ]] && [[ -z $force ]] && {
+    tcprint "error]'$target' already exists (override using --force)"
+    exit 1
+}
+[[ -d $target ]] && rm -rf $target || true
+[[ -n $verbose ]] && verbose="--verbose" || true
+mkdir $target
+
+# Flatten
+[[ -n $verbose ]] && tcprint "debug]Flattening: '$nested' => '$target'" || true
+find $nested -type f -exec cp $verbose --backup=numbered -t $target '{}' +
+[[ -n $verbose ]] && tcprint "ok]Flattened." || true
 
