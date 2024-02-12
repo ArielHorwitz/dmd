b8aa0fb3b1b1f73f0a736fd1daa04bdd25ea9674
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-02-12 15:39:29 +0200

    New script generator

---
+++ bin/newscript.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+set -e
+
+APP_NAME=$(basename "$0")
+ABOUT="Create a new executable bash script with boilerplate."
+CLI=(
+    -p "name;Script name"
+    -o "target;Target directory (will create if it doesn't exist);."
+    -f "force;Overwrite existing file;;f"
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+eval "$CLI" || exit 1
+
+# Create/find file
+filename="$name.sh"
+[[ -d $target ]] || mkdir --parents $target
+cd $target
+[[ ! -e $filename ]] || [[ -n $force ]] || exit_error "File exists, use --force to ignore this error."
+
+# Write boilerplate
+printf '#! /bin/bash
+set -e
+
+APP_NAME=$(basename "$0")
+ABOUT="DESCRIPTION"
+CLI=(
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+eval "$CLI" || exit 1' > $filename
+
+# Make executable
+chmod +x $filename
