763fd8fb7b1d62c2d909e343f34a8f40b173cccb
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-02-23 11:30:42 +0200

    openassistant usage stats

--- bin/openassistant.sh
+++ bin/openassistant.sh
@@ -2,8 +2,16 @@
 set -e
 
 API_URL="https://api.openai.com/v1/chat/completions"
+# Token costs are denominated in USD per token
+TOKEN_COST_PROMPT=0.00001
+TOKEN_COST_RESPONSE=0.00003
+
 CONFIG_DIR="$HOME/.config/openassistant"
 HISTORY_DIR="$HOME/.local/share/openassistant/history"
+KEY_FILE="$CONFIG_DIR/apikey"
+SYSTEM_INSTRUCTIONS_FILE="$CONFIG_DIR/system_instructions"
+TOTAL_USAGE_FILE="$CONFIG_DIR/usage"
+
 DEFAULT_MODEL="gpt-4-turbo-preview"
 DEFAULT_MAX_TOKENS=2048
 DEFAULT_TEMPERATURE="1"
@@ -32,12 +40,13 @@ CLI=(
     --prefix "args_"
     -O "model;Open AI gpt model;$DEFAULT_MODEL;m"
     -O "max-tokens;Maximum tokens including prompt and response;$DEFAULT_MAX_TOKENS;M"
-    -O "temperature;Model temperature sampling;$DEFAULT_TEMPERATURE;T"
-    -O "top-p;Model top-p sampling;$DEFAULT_TOP_P;O"
-    -O "frequency-penalty;Model frequency penalty;$DEFAULT_FREQUENCY_PENALTY;F"
-    -O "presence-penalty;Model presence penalty;$DEFAULT_PRESENCE_PENALTY;P"
+    -O "temperature;Model temperature sampling;$DEFAULT_TEMPERATURE"
+    -O "top-p;Model top-p sampling;$DEFAULT_TOP_P"
+    -O "frequency-penalty;Model frequency penalty;$DEFAULT_FREQUENCY_PENALTY"
+    -O "presence-penalty;Model presence penalty;$DEFAULT_PRESENCE_PENALTY"
     -O "use-history;Use previous conversation from history;;H"
     -f "list-history;List previous conversations from history and exit;;l"
+    -f "show-usage;Show usage stats and exit"
     -f "no-history;Don't save the conversation in history"
     -f "no-name;Don't add a name to the conversation in history"
     -f "quiet;Be quiet (overrides --verbose);;q"
@@ -121,8 +130,56 @@ read_response() {
     elif jq -e 'has("choices")' "$response_file" >/dev/null; then
         [[ -n $args_quiet ]] || tcprint "info bu]OpenAssistant says:"
         jq -r '.choices.[0].message.content' "$response_file" | bat -pp --language markdown
+        local cost=$(get_usage_stat $usage_file cost)
+        local tokens=$(get_usage_stat $usage_file tokens)
+        local tokens_prompt=$(get_usage_stat $usage_file tokens_prompt)
+        local tokens_response=$(get_usage_stat $usage_file tokens_response)
+        local cost_prompt=$(get_usage_stat $usage_file cost_prompt)
+        local cost_response=$(get_usage_stat $usage_file cost_response)
+        tcprint "info n]$tokens tokens (~\$$cost)"
+        tcprint "debug] [prompt: $tokens_prompt (~\$$cost_prompt)] [response: $tokens_response (~\$$cost_response)]"
     else
         jq < "$response_file"
+    fi
+}
+
+get_usage_stat() {
+    local file="$1"
+    local stat_name="$2"
+    local default=0
+    if [[ -n $3 ]]; then
+        local default="$3"
+    fi
+    local line=$(grep -E "^${stat_name}: " $file || echo "$stat_name: $default")
+    echo ${line#${stat_name}: }
+}
+
+count_usage() {
+    local add_totals="$1"
+    local tokens=$(jq '.usage.total_tokens' "$response_file")
+    local tokens_prompt=$(jq '.usage.prompt_tokens' "$response_file")
+    local tokens_response=$(jq '.usage.completion_tokens' "$response_file")
+    local cost_prompt=$(echo "scale=4;$tokens_prompt * $TOKEN_COST_PROMPT" | bc)
+    local cost_response=$(echo "scale=4;$tokens_response * $TOKEN_COST_RESPONSE" | bc)
+    local cost=$(echo "scale=4;$cost_prompt + $cost_response" | bc)
+    echo "Single exchange
+cost: $cost
+tokens: $tokens
+tokens_prompt: $tokens_prompt
+tokens_response: $tokens_response
+cost_prompt: $cost_prompt
+cost_response: $cost_response
+" > $usage_file
+    if [[ -z $args_use_history ]]; then
+        echo "TOTALS
+start: $(get_usage_stat $TOTAL_USAGE_FILE start `date +%y-%m-%d-%H-%M-%S`)
+cost: $(echo $cost + `get_usage_stat $TOTAL_USAGE_FILE cost` | bc)
+tokens: $(echo $tokens + `get_usage_stat $TOTAL_USAGE_FILE tokens` | bc)
+tokens_prompt: $(echo $tokens_prompt + `get_usage_stat $TOTAL_USAGE_FILE tokens_prompt` | bc)
+tokens_response: $(echo $tokens_response + `get_usage_stat $TOTAL_USAGE_FILE tokens_response` | bc)
+cost_prompt: $(echo $cost_prompt + `get_usage_stat $TOTAL_USAGE_FILE cost_prompt` | bc)
+cost_response: $(echo $cost_response + `get_usage_stat $TOTAL_USAGE_FILE cost_response` | bc)
+" > $TOTAL_USAGE_FILE
     fi
 }
 
@@ -141,6 +198,7 @@ save_history() {
         "$data_dir/query"
         "$data_dir/response"
         "$data_dir/headers_response"
+        "$data_dir/usage"
     )
     mkdir --parents $convo_dir
     cp -t $convo_dir ${history_files[@]}
@@ -179,32 +237,31 @@ if [[ -n $args_list_history ]]; then
     exit 0
 fi
 
-# Config files
-key_file="$CONFIG_DIR/apikey"
-sys_instruct_file="$CONFIG_DIR/system_instructions"
+# Resolve data directory and system instructions file locations
 data_dir="$HISTORY_DIR/.last"
-
+system_instructions_file=$SYSTEM_INSTRUCTIONS_FILE
 if [[ -n $args_use_history ]]; then
     convo_name=$(list_history | grep "$args_use_history" -m 1)
     [[ -n $args_quiet ]] || tcprint "notice]Using historical conversation: $convo_name"
     data_dir="$HISTORY_DIR/$convo_name"
-    sys_instruct_file="$data_dir/system_instructions"
 fi
-
+# Paths for current conversation
+system_instructions_file="$data_dir/system_instructions"
 query_file="$data_dir/query"
 response_file="$data_dir/response"
 response_headers_file="$data_dir/headers_response"
+usage_file="$data_dir/usage"
 
 [[ -d $CONFIG_DIR ]] || mkdir --parents $CONFIG_DIR
 [[ -d $data_dir ]] || mkdir --parents $data_dir
 
 # API key
-[[ -f $key_file ]] || exit_error "Missing OpenAI API key file: $key_file"
-openai_api_key="$(cat $key_file)"
+[[ -f $KEY_FILE ]] || exit_error "Missing OpenAI API key file: $KEY_FILE"
+openai_api_key="$(cat $KEY_FILE)"
 [[ -n $openai_api_key ]] || exit_error "Empty OpenAI API key"
 # System instructions
-[[ -f $sys_instruct_file ]] || echo 'You are a helpful assistant.' > $sys_instruct_file
-system_instructions="$(cat $sys_instruct_file)"
+[[ -f $system_instructions_file ]] || echo 'You are a helpful assistant.' > $system_instructions_file
+system_instructions="$(cat $system_instructions_file)"
 [[ -z $args_verbose ]] || print_sys_instructions
 # Query
 get_query
@@ -214,6 +271,7 @@ generate_call_data
 
 # Call
 [[ -n $args_use_history ]] || call_api
+count_usage
 read_response
 [[ -n $args_no_history ]] || save_history
 
