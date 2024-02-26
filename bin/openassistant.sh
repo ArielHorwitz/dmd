bfce3565687fbb1b33d32b1bc1e1b3456ec49fbb
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-02-26 12:09:57 +0200

    Rework openassistant

--- bin/openassistant.sh
+++ bin/openassistant.sh
@@ -1,15 +1,24 @@
 #! /bin/bash
 set -e
 
+# Configuration files
 CONFIG_DIR="$HOME/.config/openassistant"
-LOCAL_DIR="$HOME/.local/share/openassistant"
-HISTORY_DIR="$LOCAL_DIR/history"
 KEY_FILE="$CONFIG_DIR/apikey"
 MODEL_SETTINGS_FILE="$CONFIG_DIR/model_settings"
-SYSTEM_INSTRUCTIONS_FILE="$CONFIG_DIR/system_instructions"
-STATS_FILE="$LOCAL_DIR/stats"
-EDITOR_QUERY_BLURB='Enter your prompt here, then save and close your editor. Leave empty to cancel.'
-
+BASE_SYS_INSTR_FILE="$CONFIG_DIR/system_instructions"
+LOCAL_DIR="$HOME/.local/share/openassistant"
+HISTORY_DIR="$LOCAL_DIR/history"
+STATS_FILE_TOTALS="$LOCAL_DIR/stats"
+# Current conversation files
+CONVO_DIR="$LOCAL_DIR/.current"
+SYS_INSTR_FILE="$CONVO_DIR/system_instructions"
+QUERY_FILE="$CONVO_DIR/query"
+CALLDATA_FILE="$CONVO_DIR/calldata"
+RESPONSE_DATA_FILE="$CONVO_DIR/response_data"
+RESPONSE_HEADERS_FILE="$CONVO_DIR/response_headers"
+RESPONSE_CONTENT_FILE="$CONVO_DIR/response_content"
+STATS_FILE_CURRENT="$CONVO_DIR/stats"
+# Defaults
 API_URL="https://api.openai.com/v1/chat/completions"
 DEFAULT_MODEL_SETTINGS='# For details see: https://platform.openai.com/docs/api-reference/chat
 model: "gpt-4-turbo-preview"
@@ -18,18 +27,28 @@ temperature: 1.0
 top_p: 1.0
 frequency_penalty: .05
 presence_penalty: .01'
+DEFAULT_SYSTEM_INSTRUCTIONS='You are a helpful assistant. Format your responses in markdown.'
+EDITOR_QUERY_BLURB='Enter your prompt here, then save and close your editor. Leave empty to cancel.'
 # Token costs are denominated in USD per token (https://openai.com/pricing)
 TOKEN_COST_PROMPT=0.00001
 TOKEN_COST_RESPONSE=0.00003
 
-# Create defaults
+# Generate tree structure
 [[ -d $CONFIG_DIR ]] || mkdir --parents $CONFIG_DIR
 [[ -d $HISTORY_DIR ]] || mkdir --parents $HISTORY_DIR
+[[ -d $CONVO_DIR ]] || mkdir --parents $CONVO_DIR
+# Create defaults
 [[ -f $KEY_FILE ]] || echo "sk-YOUR_OPENAI_API_KEY" > $KEY_FILE
 [[ -f $MODEL_SETTINGS_FILE ]] || echo "$DEFAULT_MODEL_SETTINGS" > $MODEL_SETTINGS_FILE
-[[ -f $SYSTEM_INSTRUCTIONS_FILE ]] || echo 'You are a helpful assistant. Format your responses in markdown.' > $SYSTEM_INSTRUCTIONS_FILE
-[[ -f $STATS_FILE ]] || echo "No recorded stats." > $STATS_FILE
+[[ -f $BASE_SYS_INSTR_FILE ]] || echo $DEFAULT_SYSTEM_INSTRUCTIONS > $BASE_SYS_INSTR_FILE
+[[ -f $STATS_FILE_TOTALS ]] || echo "No recorded stats." > $STATS_FILE_TOTALS
 
+# Wrapper for error handling
+EXIT_ERROR="`which exit_error`"
+exit_error() {
+    cleanup
+    `$EXIT_ERROR "$@"`
+}
 
 APP_NAME=$(basename "$0")
 ABOUT='Query your personal OpenAI assistant.
@@ -37,8 +56,8 @@ ABOUT='Query your personal OpenAI assistant.
 Write your OpenAI API key in the configuration folder (--config-dir).'
 CLI=(
     --prefix "args_"
-    -O "historical;Print a conversation from history;;L"
-    -O "history;Conversation history: (d)isable, (a)uto, (n)ame;name;y"
+    -O "history;Conversation history: (Y)es, (N)o, ask-(y)es, ask-(n)o;ask-yes;y"
+    -O "load;Load a conversation index (see --list);;L"
     -f "list;List conversations from history;;l"
     -f "stats;Print recorded stats;;s"
     -f "config-dir;Print configuration directory path;;c"
@@ -49,43 +68,44 @@ CLI=(
     -f "clear-config;Delete all configuration data"
     -f "force;Do not ask for confirmation;;f"
     -f "read-stdin;Read from stdin instead of opening an editor;;I"
+    -f "debug;Enabled verbose debugging"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 eval "$CLI" || exit 1
 
-if [[ -n $args_quiet ]]; then
-    QUIET=1
-    VERBOSE=
-elif [[ -n $args_verbose ]]; then
-    QUIET=
-    VERBOSE=1
-fi
-case $args_history in
-    d | disable  ) history_enabled= ;;
-    a | auto     ) history_enabled=1 ;;
-    n | name     ) history_enabled="name" ;;
-esac
-[[ -z $args_historical ]] || history_enabled=
-
-get_query() {
-    if [[ -n $args_read_stdin ]]; then
-        tcprint "notice]Reading query from stdin..." >&2
-        read query_content
-    else
-        [[ -n $EDITOR ]] || exit_error "No editor configured (use EDITOR environment variable)"
-        if [[ -z $args_historical ]]; then
-            [[ -z $VERBOSE ]] || tcprint "notice]Reading query from editor: \"$EDITOR\"" >&2
-            [[ -f $query_file && -n $(< $query_file) ]] || echo $EDITOR_QUERY_BLURB > $query_file
-            `$EDITOR $query_file` &>/dev/null
-        fi
-        query_content="$(cat $query_file)"
-    fi
+get_api_key() {
+    [[ -f $KEY_FILE ]] || exit_error "Missing OpenAI API key file: $KEY_FILE"
+    openai_api_key="$(cat $KEY_FILE)"
+    [[ -n $openai_api_key ]] || exit_error "Empty OpenAI API key: $KEY_FILE"
 }
 
 get_model_setting() {
     local stat_name="$1"
     local line=$(grep -E "^${stat_name}: " $MODEL_SETTINGS_FILE) || exit_error "Missing model setting: $stat_name. Delete $MODEL_SETTINGS_FILE to regenerate defaults."
     printf "%s" "${line#${stat_name}: }"
+}
+
+get_system_instructions() {
+    [[ -f $SYS_INSTR_FILE ]] || cp $BASE_SYS_INSTR_FILE $SYS_INSTR_FILE
+    system_instructions="$(cat $SYS_INSTR_FILE)"
+}
+
+get_query() {
+    query_content="$(cat $QUERY_FILE)"
+    [[ -n $query_content ]] || exit_error "Query content is empty"
+}
+
+read_query_to_file() {
+    if [[ -n $args_read_stdin ]]; then
+        [[ -n $QUIET ]] || tcprint "notice]Reading query from stdin..." >&2
+        read query_content
+        echo "$query_content" > $QUERY_FILE
+    else
+        [[ -n $EDITOR ]] || exit_error "No editor configured (use EDITOR environment variable)"
+        [[ -z $VERBOSE ]] || tcprint "notice]Reading query from editor: \"$EDITOR\"" >&2
+        [[ -f $QUERY_FILE && -n $(< $QUERY_FILE) ]] || echo $EDITOR_QUERY_BLURB > $QUERY_FILE
+        `$EDITOR $QUERY_FILE` &>/dev/null
+    fi
 }
 
 generate_call_data() {
@@ -118,15 +138,15 @@ generate_call_data() {
         --argjson presence_penalty $(get_model_setting presence_penalty)
     )
     api_call_data=$(jq -n "${jq_args[@]}" "$json_template")
-    echo "$api_call_data" > $calldata_file
+    echo "$api_call_data" > $CALLDATA_FILE
 }
 
 call_api() {
     local curl_args=(
         "$API_URL"
         --silent --show-error
-        -o "$response_file"
-        -D "$response_headers_file"
+        -o "$RESPONSE_DATA_FILE"
+        -D "$RESPONSE_HEADERS_FILE"
         -H "Content-Type: application/json"
         -H "Authorization: Bearer $openai_api_key"
         -d "$api_call_data"
@@ -136,26 +156,29 @@ call_api() {
     printf         '\r                    \r'
 }
 
+read_response() {
+    local raw_response=$(jq -r '.choices.[0].message.content' "$RESPONSE_DATA_FILE" || echo '')
+    if [[ -n $raw_response ]]; then
+        echo "$raw_response" > $RESPONSE_CONTENT_FILE
+        return 0
+    fi
+    tcprint "yellow bu]Error:" >&2
+    jq --color-output < "$RESPONSE_DATA_FILE" >&2
+    exit_error "$(jq -r '.error.message' $RESPONSE_DATA_FILE)"
+}
+
 print_response() {
-    if jq -e 'has("error")' "$response_file" >/dev/null; then
-        tcprint "yellow bu]Error:" >&2
-        jq --color-output < "$response_file" >&2
-        exit_error "$(jq -r '.error.message' "$response_file")"
-    elif jq -e 'has("choices")' "$response_file" >/dev/null; then
-        [[ -n $QUIET ]] || tcprint "green bu]OpenAssistant says:"
-        jq -r '.choices.[0].message.content' "$response_file" | bat -pp --color always --language markdown
-        if [[ -z $QUIET ]]; then
-            local cost=$(get_stat_current cost)
-            local tokens=$(get_stat_current tokens)
-            local tokens_prompt=$(get_stat_current tokens_prompt)
-            local tokens_response=$(get_stat_current tokens_response)
-            local cost_prompt=$(get_stat_current cost_prompt)
-            local cost_response=$(get_stat_current cost_response)
-            tcprint "cyan bn]$tokens tokens ~\$$cost"
-            tcprint "cyan bd] [prompt: $tokens_prompt ~\$$cost_prompt] [response: $tokens_response ~\$$cost_response]"
-        fi
-    else
-        jq --color-output < "$response_file"
+    [[ -n $QUIET ]] || tcprint "green bu]OpenAssistant says:"
+    bat -pp --color always --language markdown $RESPONSE_CONTENT_FILE
+    if [[ -z $QUIET ]]; then
+        local cost=$(get_stat_current cost)
+        local tokens=$(get_stat_current tokens)
+        local tokens_prompt=$(get_stat_current tokens_prompt)
+        local tokens_response=$(get_stat_current tokens_response)
+        local cost_prompt=$(get_stat_current cost_prompt)
+        local cost_response=$(get_stat_current cost_response)
+        tcprint "cyan bn]$tokens tokens ~\$$cost"
+        tcprint "cyan bd] [prompt: $tokens_prompt ~\$$cost_prompt] [response: $tokens_response ~\$$cost_response]"
     fi
 }
 
@@ -163,21 +186,21 @@ get_stat_global() {
     local stat_name="$1"
     local default
     [[ -n $2 ]] && default="$2" || default=0
-    local line=$(grep -E "^${stat_name}: " $STATS_FILE || echo "$stat_name: $default")
+    local line=$(grep -E "^${stat_name}: " $STATS_FILE_TOTALS || echo "$stat_name: $default")
     echo ${line#${stat_name}: }
 }
 
 get_stat_current() {
     local stat_name="$1"
     local default=0
-    local line=$(grep -E "^${stat_name}: " $stats_file_current || echo "$stat_name: $default")
+    local line=$(grep -E "^${stat_name}: " $STATS_FILE_CURRENT || echo "$stat_name: $default")
     echo ${line#${stat_name}: }
 }
 
 tally_stats() {
-    local tokens=$(jq '.usage.total_tokens' "$response_file")
-    local tokens_prompt=$(jq '.usage.prompt_tokens' "$response_file")
-    local tokens_response=$(jq '.usage.completion_tokens' "$response_file")
+    local tokens=$(jq '.usage.total_tokens' "$RESPONSE_DATA_FILE")
+    local tokens_prompt=$(jq '.usage.prompt_tokens' "$RESPONSE_DATA_FILE")
+    local tokens_response=$(jq '.usage.completion_tokens' "$RESPONSE_DATA_FILE")
     local cost_prompt=$(echo "scale=4;$tokens_prompt * $TOKEN_COST_PROMPT" | bc)
     local cost_response=$(echo "scale=4;$tokens_response * $TOKEN_COST_RESPONSE" | bc)
     local cost=$(echo "scale=4;$cost_prompt + $cost_response" | bc)
@@ -189,9 +212,8 @@ tokens_prompt: $tokens_prompt
 tokens_response: $tokens_response
 cost_prompt: $cost_prompt
 cost_response: $cost_response" \
-    > $stats_file_current
-    if [[ -z $args_historical ]]; then
-        echo \
+    > $STATS_FILE_CURRENT
+    echo \
 "STATS - TOTALS
 start: $(get_stat_global start `date +%y-%m-%d-%H-%M-%S`)
 cost: $(echo $cost + `get_stat_global cost` | bc)
@@ -200,46 +222,62 @@ tokens_prompt: $(echo $tokens_prompt + `get_stat_global tokens_prompt` | bc)
 tokens_response: $(echo $tokens_response + `get_stat_global tokens_response` | bc)
 cost_prompt: $(echo $cost_prompt + `get_stat_global cost_prompt` | bc)
 cost_response: $(echo $cost_response + `get_stat_global cost_response` | bc)" \
-        > $STATS_FILE
-    fi
+    > $STATS_FILE_TOTALS
 }
 
 save_history() {
-    local current_date=`date +%y-%m-%d-%H-%M-%S`
-    local convo_name=$current_date
-    local user_given_convo_name
-    if [[ $history_enabled = "name" ]]; then
-        tcprint "green d][Enter '.' to skip saving]" >&2
-        tcprint "notice n]Conversation name: " >&2
-        read user_given_convo_name
-        [[ $user_given_convo_name != "." ]] || return 0
-        if [[ -n $(xargs <<< $user_given_convo_name) ]]; then
-            user_given_convo_name=$(sed 's/ /-/g' <<< $user_given_convo_name)
-            convo_name="${current_date}__${user_given_convo_name}"
-        fi
-    fi
-    local convo_dir="$HISTORY_DIR/$convo_name"
+    case $save_history_enabled in
+        1   ) : ;;
+        ay  ) promptconfirm "Save history?" || return 0 ;;
+        an  ) promptconfirm -d "Save history?" || return 0 ;;
+        *   ) return 0 ;;
+    esac
+    local convo_dir="$HISTORY_DIR/`date +%y-%m-%d-%H-%M-%S`"
     local history_files=(
-        $system_instructions_file
-        $query_file
-        $calldata_file
-        $response_file
-        $response_headers_file
-        $stats_file_current
+        $SYS_INSTR_FILE
+        $QUERY_FILE
+        $CALLDATA_FILE
+        $RESPONSE_DATA_FILE
+        $RESPONSE_HEADERS_FILE
+        $RESPONSE_CONTENT_FILE
+        $STATS_FILE_CURRENT
     )
     mkdir --parents $convo_dir
     cp -t $convo_dir ${history_files[@]}
 }
 
-remove_last_query_data() {
-    rm -rf $HISTORY_DIR/.last
+cleanup() {
+    local delete_files=(
+        $SYS_INSTR_FILE
+        $CALLDATA_FILE
+        $RESPONSE_DATA_FILE
+        $RESPONSE_HEADERS_FILE
+        $RESPONSE_CONTENT_FILE
+        $STATS_FILE_CURRENT
+    )
+    rm -f ${delete_files[@]}
+}
+
+clear_data() {
+    if [[ -z $args_force ]]; then
+        promptconfirm -d "Clear all recorded stats and history?" || exit_error "Aborted."
+    fi
+    rm -rf $HISTORY_DIR
+    rm -f $STATS_FILE_TOTALS
+}
+
+clear_config() {
+    if [[ -z $args_force ]]; then
+        promptconfirm -d "Clear all configuration data?" || exit_error "Aborted."
+    fi
+    rm -rf $CONFIG_DIR
 }
 
 print_debug_prequery() {
     tcprint "green bu]Environment:"
-    tcprint "green dn]    API key: "; echo "${openai_api_key:0:8}..."
     tcprint "green dn] Config dir: "; echo $CONFIG_DIR
-    tcprint "green dn]   Data dir: "; echo $data_dir
+    tcprint "green dn]  Local dir: "; echo $LOCAL_DIR
+    tcprint "green dn]    API key: "; echo "${openai_api_key:0:8}..."
     tcprint "green bu]System instructions:"
     tcset "yellow d"
     printf "%s\n" "$system_instructions"
@@ -258,87 +296,103 @@ print_query() {
 
 print_debug() {
     tcprint "green bu]Response headers:"
-    bat -pp --color always "$response_headers_file"
+    bat -pp --color always "$RESPONSE_HEADERS_FILE"
+}
+
+load_from_history() {
+    local dirs
+    mapfile dirs <<< `list_conversations`
+    local dirname=`echo "${dirs[args_load]}" | xargs`
+    local dir="$HISTORY_DIR/$dirname"
+    [[ -n $dirname && -d $dir ]] || exit_error "Unknown conversation index: $args_load"
+    [[ -n $QUIET ]] || tcprint "notice]Loading conversation from: $dirname"
+    cp -t $CONVO_DIR "$dir"/*
+}
+
+list_conversations() {
+    ls -1 $HISTORY_DIR | sort -r
 }
 
 list_history() {
-    if [[ -d "$HISTORY_DIR" ]]; then
-        find "$HISTORY_DIR" -mindepth 1 -maxdepth 1 -type d -printf '%P\n' | sort -r
-    fi
+    local dirs
+    local query_cols=$((`tput cols` - 19))
+    local query_cap=$((query_cols - 5))
+    mapfile dirs <<< `list_conversations`
+    [[ ${#dirs[@]} -gt 0 ]] || return 0
+    for index in ${!dirs[@]}; do
+        tcprint "purple dn]`printf %-2s $index` "
+        local dir=`echo "$HISTORY_DIR/${dirs[index]}" | xargs`
+        local fulldate=`basename $dir`
+        tcprint "blue dn]`printf '%-9s' ${fulldate:0:8}`"
+        local dtime=`cut -d- -f4- <<< $fulldate | sed 's/-/:/g'`
+        tcprint "blue dn]`printf '%-9s' ${dtime:0:8}` "
+        # local ptokens=$(printf '%3s' $(wc -w "$dir/query" 2>&- | awk '{print $1}' || printf "??"))
+        # local rtokens=$(printf '%3s' $(wc -w "$dir/response_content" 2>&- | awk '{print $1}' || printf "??"))
+        # tcprint "red dn]$ptokens "
+        # tcprint "green dn]$rtokens "
+        local query_line=$(tr '\n' ' ' < "$dir/query" || echo "??")
+        [[ ${#query_line} -le $query_cols ]] || local query_line="${query_line:0:query_cap}.."
+        tcprint "cyan dn]$query_line"
+        echo
+    done
 }
 
-# Simple short operations
-if [[ -n $args_version ]]; then
-    echo $DATA_VERSION
-    exit 0
-elif [[ -n $args_list ]]; then
-    list_history
-    exit 0
-elif [[ -n $args_config_dir ]]; then
-    echo $CONFIG_DIR
-    exit 0
-elif [[ -n $args_history_dir ]]; then
-    echo $HISTORY_DIR
-    exit 0
-elif [[ -n $args_stats ]]; then
-    cat $STATS_FILE
-    exit 0
-elif [[ -n $args_clear_data ]]; then
-    if [[ -z $args_force ]]; then
-        promptconfirm -d "Clear all recorded stats and history?" || exit_error "Aborted."
-    fi
-    rm -rf $HISTORY_DIR
-    rm -f $STATS_FILE
-    exit 0
-elif [[ -n $args_clear_config ]]; then
-    if [[ -z $args_force ]]; then
-        promptconfirm -d "Clear all configuration data?" || exit_error "Aborted."
-    fi
-    rm -rf $CONFIG_DIR
-    exit 0
-fi
+flow_query_normal() {
+    get_api_key
+    get_system_instructions
+    [[ -z $VERBOSE ]] || print_debug_prequery
 
-# Resolve data directory and system instructions file locations
-data_dir="$HISTORY_DIR/.last"
-if [[ -n $args_historical ]]; then
-    convo_name=$(list_history | grep "$args_historical" -m 1) || exit_error "No conversations found: '$args_historical'"
-    [[ -n $QUIET ]] || tcprint "notice]Using historical conversation: $convo_name"
-    data_dir="$HISTORY_DIR/$convo_name"
-fi
-[[ -d $data_dir ]] || mkdir --parents $data_dir
-# Current conversation data paths
-system_instructions_file="$data_dir/system_instructions"
-cp $SYSTEM_INSTRUCTIONS_FILE $system_instructions_file
-query_file="$data_dir/query"
-calldata_file="$data_dir/calldata"
-response_file="$data_dir/response"
-response_headers_file="$data_dir/headers_response"
-stats_file_current="$data_dir/stats"
+    read_query_to_file
+    get_query
+    [[ -n $QUIET ]] || print_query
 
-# System instructions
-system_instructions="$(cat $system_instructions_file)"
+    generate_call_data
+    [[ -z $VERBOSE ]] || print_debug_precall
+    call_api
+    read_response
+    tally_stats
 
-# API key
-[[ -f $KEY_FILE ]] || exit_error "Missing OpenAI API key file: $KEY_FILE"
-openai_api_key="$(cat $KEY_FILE)"
-[[ -n $openai_api_key ]] || exit_error "Empty OpenAI API key"
-
-# Get query
-[[ -z $VERBOSE ]] || print_debug_prequery
-get_query
-[[ -n $query_content ]] || exit_error "Query content is empty"
-generate_call_data
-[[ -n $QUIET ]] || print_query
-
-# Call API
-[[ -z $VERBOSE ]] || print_debug_precall
-[[ -n $args_historical ]] || call_api
-tally_stats
-[[ -z $VERBOSE ]] || print_debug
-print_response
-if [[ -n $history_enabled ]]; then
+    [[ -z $VERBOSE ]] || print_debug
+    print_response
     save_history
-else
-    remove_last_query_data
-fi
+    cleanup
+}
 
+flow_query_from_history() {
+    load_from_history
+    get_system_instructions
+    [[ -z $VERBOSE ]] || print_debug_prequery
+    get_query
+    [[ -n $QUIET ]] || print_query
+    [[ -z $VERBOSE ]] || print_debug_precall
+    [[ -z $VERBOSE ]] || print_debug
+    print_response
+    cleanup
+}
+
+# Start
+[[ -z $args_debug ]] || set -x
+if [[ -n $args_quiet ]]; then
+    QUIET=1
+    VERBOSE=
+elif [[ -n $args_verbose ]]; then
+    QUIET=
+    VERBOSE=1
+fi
+case ${args_history} in
+    n | ask-no     ) save_history_enabled="an" ;;
+    y | ask-yes    ) save_history_enabled="ay" ;;
+    Y | Yes | yes  ) save_history_enabled=1 ;;
+    N | No | no    ) save_history_enabled= ;;
+    *              ) exit_error "Invalid history option: $args_history" ;;
+esac
+
+if [[ -n $args_list ]];            then list_history
+elif [[ -n $args_config_dir ]];    then echo $CONFIG_DIR
+elif [[ -n $args_history_dir ]];   then echo $HISTORY_DIR
+elif [[ -n $args_stats ]];         then cat $STATS_FILE_TOTALS
+elif [[ -n $args_clear_data ]];    then clear_data
+elif [[ -n $args_clear_config ]];  then clear_config
+elif [[ -n $args_load ]];    then flow_query_from_history
+else flow_query_normal
+fi
