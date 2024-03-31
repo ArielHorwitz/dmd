24692b7789ee5fd3eb5d8610dd336e85a1b48c61
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-03-31 13:20:37 +0300

    Add configurable offset to mouse

--- bin/mouse.sh
+++ bin/mouse.sh
@@ -7,6 +7,7 @@ APP_NAME=$(basename "$0")
 ABOUT="Control the mouse"
 CLI=(
     -O "click;Click mouse button by number;;C"
+    -O "edge-offset;Pixel offset when moving mouse to edge;1;O"
     -f "center;Move mouse to window center;;c"
     -f "top;Move mouse to window top edge;;t"
     -f "bottom;Move mouse to window bottom edge;;b"
@@ -20,12 +21,12 @@ eval $(xdotool getactivewindow getwindowgeometry --shell)
 
 if [[ -n "$center$top$bottom$left$right" ]]; then
     mx=$(( X + WIDTH / 2 ))
-    [[ -z $right ]] || mx=$(( X + WIDTH - 1 ))
-    [[ -z $left ]] || mx=$(( X + 1 ))
+    [[ -z $right ]] || mx=$(( X + WIDTH - edge_offset ))
+    [[ -z $left ]] || mx=$(( X + edge_offset ))
 
     my=$(( Y + HEIGHT / 2 ))
-    [[ -z $bottom ]] || my=$(( Y + HEIGHT - 1 ))
-    [[ -z $top ]] || my=$(( Y + 1 ))
+    [[ -z $bottom ]] || my=$(( Y + HEIGHT - edge_offset ))
+    [[ -z $top ]] || my=$(( Y + edge_offset ))
 
     xdotool mousemove $mx $my
 fi
