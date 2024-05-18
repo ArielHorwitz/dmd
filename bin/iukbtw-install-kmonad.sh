cc1556a808270e7f7f13e21d8458d5afa5203448
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-05-18 20:07:04 +0300

    Use sudo flag instead of bash workaround

--- bin/iukbtw-install-kmonad.sh
+++ bin/iukbtw-install-kmonad.sh
@@ -8,7 +8,7 @@ BINARY_URL="https://github.com/kmonad/kmonad/releases/download/0.4.2/kmonad"
 
 # Root
 [[ $EUID -eq 0 ]] && exit_error "Do not run `basename $0` as root."
-sudo :
+sudo -v
 printcolor -s ok "Root privileges aquired."
 
 setup_installation() {
