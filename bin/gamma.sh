ae31e2ffc9ffb83d664acf6b34dc153e359810b1
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-07-14 14:42:14 +0300

    Update gamma values to be more visible

--- bin/gamma.sh
+++ bin/gamma.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-H=1.0
-M=0.9
-L=0.7
+H=1.5
+M=1.0
+L=0.8
 case $1 in
     bright    ) gamma=$H:$H:$H ;;
     dark      ) gamma=$L:$L:$L ;;
@@ -10,7 +10,7 @@ case $1 in
     green     ) gamma=$M:$H:$M ;;
     blue      ) gamma=$M:$M:$H ;;
     orange    ) gamma=$H:$M:$L ;;
-    cyan      ) gamma=$L:$H:$M ;;
+    cyan      ) gamma=$L:$H:$H ;;
     purple    ) gamma=$M:$L:$H ;;
     *         ) gamma=1 ;;
 esac
