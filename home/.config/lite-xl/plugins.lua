dc702d2bb26e8ee2f4ebfeda43043976a1eaa467
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-08-31 22:18:47 +0300

    Disable linewrapping by default

--- home/.config/lite-xl/plugins.lua
+++ home/.config/lite-xl/plugins.lua
@@ -12,7 +12,7 @@ config.plugins.drawwhitespace = { enabled = true, show_leading = false, show_mid
 config.plugins.lineguide.width = 3
 config.plugins.lineguide.enabled = true
 config.plugins.lineguide.rulers = { [1] = 80, [2] = 100, [3] = 120 }
-config.plugins.linewrapping.enable_by_default = true
+config.plugins.linewrapping.enable_by_default = false
 config.plugins.linewrapping.mode = "word"
 config.plugins.lsp.mouse_hover_delay = 750
 config.plugins.lsp.more_yielding = true
