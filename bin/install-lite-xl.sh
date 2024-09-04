95be191c49ff0d9a957d789e247bdc676be26a53
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-09-04 10:35:57 +0300

    Rework lite-xl installer

---
+++ bin/install-lite-xl.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/bash
+set -e
+
+CONFIG_DIR="$HOME/.config/lite-xl/"
+DATA_DIR="$HOME/.local/share/lite-xl/"
+BIN_DIR="$HOME/.local/bin/"
+TMPDIR="/tmp/install-lite-xl/"
+
+PLUGINS_FILE="$HOME/.config/litexl-installer-plugins.txt"
+
+
+APP_NAME=$(basename "$0")
+ABOUT="Install lite-xl and lpm."
+CLI=(
+    --prefix "args_"
+    -O "plugins-file;File specifying which plugins to install using lpm;$PLUGINS_FILE"
+    -f "clear;Clear existing installation"
+    -f "full;Clear and install everything"
+    -f "app;Install/update lite-xl"
+    -f "lpm;Install lpm"
+    -f "lsp;Install lsp languages"
+    -f "plugins;Install plugins"
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+eval "$CLI" || exit 1
+
+
+install_lite_xl() {
+    sudo -v
+    cd $TMPDIR
+
+    printcolor -s ok "Downloading latest release..."
+    asset_pattern="*addons-linux-x86_64-portable.tar.gz"
+    downloaded_filename="lite-xl.tar.gz"
+    gh release download --repo 'lite-xl/lite-xl' -p $asset_pattern -O $downloaded_filename
+
+    printcolor -s ok "Extracting..."
+    tar -xzf $downloaded_filename
+    cd lite-xl
+
+    printcolor -s ok "Installing binary..."
+    install -Dt $BIN_DIR lite-xl
+
+    printcolor -s ok "Replacing existing plugins..."
+    rm -rf $DATA_DIR
+    mkdir -p $DATA_DIR
+    mv -f data/* $DATA_DIR
+}
+
+update_xdg_menus() {
+    printcolor -s ok "Populating XDG application..."
+    DESKTOP_APPS="$HOME/.local/share/applications/"
+    mkdir -p $DESKTOP_APPS
+    echo '[Desktop Entry]
+    Type=Application
+    Name=Lite XL
+    Comment=A lightweight text editor written in Lua
+    Exec=lite-xl %F
+    # Icon=lite-xl
+    Terminal=false
+    StartupWMClass=lite-xl
+    Categories=Development;IDE;
+    MimeType=text/plain;inode/directory;' > $DESKTOP_APPS/lite-xl.desktop
+    # DESKTOP_ICONS="$HOME/.local/share/icons/hicolor/48x48/apps/"
+    # cp lite-xl.png
+    xdg-desktop-menu forceupdate
+}
+
+install_lpm() {
+    cd $TMPDIR
+
+    printcolor -s ok "Installing 'lpm' plugin manager..."
+    URL="https://github.com/lite-xl/lite-xl-plugin-manager/releases/download/latest/lpm.x86_64-linux"
+    curl -sSL $URL -o lpm
+    install -Dt $BIN_DIR lpm
+}
+
+install_plugins() {
+    printcolor -s ok "Installing plugins..."
+    lpm install --assume-yes $(< $args_plugins_file)
+}
+
+install_lsp_languages() {
+    cd $TMPDIR
+    printcolor -s ok "Installing language servers..."
+
+    printcolor -s ok "Installing Python LSP..."
+    python -m pip install -q --break-system-packages python-lsp-server
+    printcolor -s ok "Installing Rust LSP..."
+    [[ $(command -v rust-analyzer) ]] || rustup component add rust-analyzer
+    printcolor -s ok "Installing Lua LSP..."
+    sudo pacman -Sq --needed --noconfirm lua-language-server
+    printcolor -s ok "Installing Bash LSP..."
+    sudo npm install --silent --no-progress --global bash-language-server
+    printcolor -s ok "Installing Dockerfile LSP..."
+    sudo npm install --silent --no-progress --global dockerfile-language-server-nodejs
+}
+
+printcolor -s info "Temporary working directory: $TMPDIR"
+rm -rf $TMPDIR
+mkdir -p $TMPDIR
+
+if [[ $args_full || $args_clear ]]; then
+    rm $BIN_DIR/lite-xl
+    rm -rf $DATA_DIR
+    rm -rf $CONFIG_DIR
+fi
+if [[ $args_full || $args_app ]]; then
+    install_lite_xl
+    update_xdg_menus
+fi
+if [[ $args_full || $args_lpm ]]; then
+    install_lpm
+fi
+if [[ $args_full || $args_plugins ]]; then
+    install_plugins
+fi
+if [[ $args_full || $args_lsp ]]; then
+    install_lsp_languages
+fi
+
+printcolor -s ok "Cleaning up temporary working directory $TMPDIR"
+rm -rf $TMPDIR
