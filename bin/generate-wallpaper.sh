c5d412ea09274ccb3d01f8ef8ef358d84c5a588e
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-09-08 22:12:16 +0300

    Wallpaper generator

---
+++ bin/generate-wallpaper.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -e
+
+cfonts "|linux|" \
+    --spaceless \
+    --font simple \
+    --line-height 3 \
+    --align center \
+    --colors "#30f6e0"
+
+cfonts "|where there is a shell|there is a way||... and i have a shell" \
+    --spaceless \
+    --font tiny \
+    --line-height 3 \
+    --align center \
+    --colors "#08aa08"
+
+echo
