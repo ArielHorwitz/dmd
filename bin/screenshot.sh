1209a61089c50eef87da32a28ddd95a15d8a961f
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-09-27 22:23:08 +0300

    Update screenshot script

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,4 +1,32 @@
-#!/bin/bash
+#! /bin/bash
+set -e
 
-mkdir --parents "/home/$USER/temp/screen/"
-scrot $@ -uf /home/$USER/temp/screen/%Y-%m-%d_%H:%M:%S.png -e "userprompt -p 'Screenshot name: ' -e 'mv \$f /home/$USER/temp/screen/%Y-%m-%d_%H:%M:%S_{{}}.png'"
+# CLI
+APP_NAME=$(basename "${0%.*}")
+ABOUT="Take a screenshot"
+CLI=(
+    --prefix "args_"
+    -f "desktop;Capture the entire desktop, not just focused window;;d"
+    -f "prompt;Prompt for screenshot name;;p"
+    -O "name;Screenshot name;;n"
+    -O "targetdir;Target directory to save to;$HOME/temp/screen;t"
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+# echo "$CLI" >&2
+eval "$CLI" || exit 1
+
+temp_file="$(mktemp --dry-run).png"
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S')
+if [[ $args_prompt ]]; then
+    name=$(terminalprompt 'Screenshot name: ')
+else
+    name=$args_name
+fi
+target_file="${args_targetdir}/${timestamp}${name:+_$name}.png"
+mkdir --parents $args_targetdir
+
+scrot_args=(--file $temp_file)
+[[ $args_desktop ]] || scrot_args+=(--focused)
+
+scrot ${scrot_args[@]}
+mv $temp_file $target_file
