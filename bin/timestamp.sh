d7065ad2894e0f7f788b9739ae72316d2764c10f
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-11-02 16:23:45 +0200

    Enable sub-seconds

--- bin/timestamp.sh
+++ bin/timestamp.sh
@@ -9,6 +9,8 @@ CLI=(
     -f "no-date;Do not include date;;d"
     -f "no-time;Do not include hours/minutes;;t"
     -f "no-seconds;Do not include seconds;;s"
+    -f "milliseconds;Include milliseconds;;m"
+    -f "microseconds;Include microseconds;;c"
     -f "clipboard;Copy to clipboard;;C"
     -O "num-delim;Delimiter between numbers;-"
     -O "date-delim;Delimiter between date and time;__"
@@ -16,7 +18,6 @@ CLI=(
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 # echo "$CLI" >&2
 eval "$CLI" || exit 1
-
 
 format=""
 if [[ -z $args_no_date ]]; then
@@ -26,6 +27,11 @@ if [[ -z $args_no_time ]]; then
     format="${format:+${format}${args_date_delim}}%H${args_num_delim}%M"
     if [[ -z $args_no_seconds ]]; then
         format+="${args_num_delim}%S"
+        if [[ $args_microseconds ]]; then
+            format+="${args_num_delim}$(date '+%N' | cut -b-6)"
+        elif [[ $args_milliseconds ]]; then
+            format+="${args_num_delim}$(date '+%N' | cut -b-3)"
+        fi
     fi
 fi
 [[ $format ]] || exit_error "No format options selected"
