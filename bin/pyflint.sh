f3bf85dd5ec113c18f2123fdc62b33b8aeee467c
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2024-11-07 23:23:37 +0200

    Enable skipping missing commands

--- bin/pyflint.sh
+++ bin/pyflint.sh
@@ -9,6 +9,7 @@ CLI=(
     -O "line-length;Maximum line length;88"
     -f "clear;Clear the terminal before running;;c"
     -f "strict;Run MyPy in strict mode;;s"
+    -f "warn;Warn on missing commands;;w"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 # echo "$CLI" >&2
@@ -16,20 +17,43 @@ eval "$CLI" || exit 1
 
 if [[ -n $args_clear ]]; then clear; fi
 
-# Formatters
-printcolor -fc "=== isort ==="
-isort --line-length $args_line_length --profile black $args_target
+warn() {
+    set -e
+    if [[ $args_warn ]]; then
+        printcolor -s warn "missing $1"
+    fi
+}
 
-printcolor -fc "=== Black ==="
-black --check --line-length $args_line_length --fast $args_target
+# Formatters
+if command -v isort >/dev/null; then
+    printcolor -fc "=== isort ==="
+    isort --line-length $args_line_length --profile black $args_target
+else
+    warn "isort"
+fi
+
+if command -v black >/dev/null; then
+    printcolor -fc "=== Black ==="
+    black --check --line-length $args_line_length --fast $args_target
+else
+    warn "black"
+fi
 
 # Linters
-printcolor -fm "=== MyPy ==="
-mypy_args=(
-    --exclude 'venv|.venv'
-    `[[ -z $args_strict ]] || echo --strict`
-)
-mypy ${mypy_args[@]} $args_target
+if command -v mypy >/dev/null; then
+    printcolor -fm "=== MyPy ==="
+    mypy_args=(
+        --exclude 'venv|.venv'
+        `[[ -z $args_strict ]] || echo --strict`
+    )
+    mypy ${mypy_args[@]} $args_target
+else
+    warn "mypy"
+fi
 
-printcolor -fm "=== Flake8 ==="
-flake8 --max-line-length $args_line_length --extend-exclude 'venv,.venv' $args_target
+if command -v flake8 >/dev/null; then
+    printcolor -fm "=== Flake8 ==="
+    flake8 --max-line-length $args_line_length --extend-exclude 'venv,.venv' $args_target
+else
+    warn "flake8"
+fi
