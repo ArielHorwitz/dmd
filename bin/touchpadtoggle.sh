f8690c3c747762c7fecaab2b4cb58e0ddedc0378
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-01-06 12:05:58 +0200

    Enabled status notification

--- bin/touchpadtoggle.sh
+++ bin/touchpadtoggle.sh
@@ -7,7 +7,7 @@ CLI=(
     --prefix "args_"
     -f "on;Enable touchpad"
     -f "off;Disable touchpad"
-    -f "status;Print enabled status;;s"
+    -f "no-notification;Disable notification of enabled status;;n"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 eval "$CLI" || exit 1
@@ -21,7 +21,7 @@ get_status() {
 }
 
 if [[ $args_status ]]; then
-    if $(get_status); then
+    if get_status; then
         echo "Touchpad enabled"
         exit 0
     else
@@ -36,18 +36,24 @@ if [[ -n $args_on ]]; then
 elif [[ -n $args_off ]]; then
     SETAS="off"
 else
-    if $(get_status); then
+    if get_status; then
         SETAS="off"
     else
         SETAS="on"
     fi
 fi
 
+notification_args=(-u low -t 1500 -h string:synchronous:touchpadtoggle)
+
 # Set property
 if [[ $SETAS == "on" ]]; then
-    xinput --set-prop $DEVICE "Device Enabled" 1
+    xinput --set-prop "$DEVICE" "Device Enabled" 1
+    echo "Enabled touchpad"
+    [[ $args_no_notification ]] || notify-send "Enabled touchpad" "${notification_args[@]}"
 elif [[ $SETAS == "off" ]]; then
-    xinput --set-prop $DEVICE "Device Enabled" 0
+    xinput --set-prop "$DEVICE" "Device Enabled" 0
+    echo "Disabled touchpad"
+    [[ $args_no_notification ]] || notify-send "Disabled touchpad" "${notification_args[@]}"
 else
     exit_error "Unknown property value: $SETAS"
 fi
