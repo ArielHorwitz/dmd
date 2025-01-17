5e44de006cfa2c4cb365f6732dd16607fc0812d0
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-01-17 10:41:01 +0200

    Remove gamma from layers

--- bin/setlayer.sh
+++ bin/setlayer.sh
@@ -6,11 +6,9 @@ keyboard_layout=$2
 if [[ $layer = "base" ]]; then
     setxkbmap us
     polybar-msg action kmd hook 0
-    gamma &
 elif [[ $layer = "text" ]]; then
     setxkbmap $keyboard_layout
     polybar-msg action kmd hook 1
-    gamma -r 0.8 -g 1.5 -b 1.5 &
 else
     echo "No such layer '$layer'" >&2; exit 1
 fi
