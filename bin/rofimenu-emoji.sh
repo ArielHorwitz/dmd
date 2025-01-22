991da378a5519e9af3d52ab2f17f0599c28d7f1f
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-01-23 00:23:19 +0200

    Implement emoji picker

---
+++ bin/rofimenu-emoji.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+set -e
+
+DATA_DIR="${HOME}/.local/share/rofimenu-emoji"
+EMOJIS_DATA="${DATA_DIR}/emojis.json"
+
+# CLI
+APP_NAME=$(basename "$0")
+ABOUT="Select emojis from rofi"
+CLI=(
+    --prefix "args_"
+    -o "emoji;Selected emoji"
+    -f "update;Update emoji data"
+    -O "mode;Mode of descriptions [full, tags, groups, labels];full;m"
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+# echo "$CLI" >&2
+eval "$CLI" || exit 1
+
+if [[ $args_update ]] || [[ ! -f "$EMOJIS_DATA" ]] ; then
+    mkdir -p "$DATA_DIR"
+    curl -sSL https://www.emoji.family/api/emojis -o "$EMOJIS_DATA"
+    printcolor -s ok "Updated emojis: $EMOJIS_DATA"
+fi
+
+case $args_mode in
+    full   ) jq_filter='.[] | .emoji + "  " + .annotation + " [" + .group + " / " + .subgroup + "]  " + (.tags | .? | map("#" + .) | join(" "))'  ;;
+    tags   ) jq_filter='.[] | .emoji + "  " + .annotation + " " + (.tags | .? | map("#" + .) | join(" "))'  ;;
+    groups ) jq_filter='.[] | .emoji + "  " + .annotation + " [" + .group + " / " + .subgroup + "]"'  ;;
+    labels ) jq_filter='.[] | .emoji + "  " + .annotation'  ;;
+    *      ) exit_error "Unknown mode: $args_mode"
+esac
+
+emojis=$(jq -r "$jq_filter" < "$EMOJIS_DATA")
+
+if [[ $args_emoji ]]; then
+    emoji=$(printf "%s" "$args_emoji" | awk '{ printf "%s", $1 }')
+    printf '%s' "$emoji" | xsel -ib
+    exit
+fi
+
+echo "$emojis"
