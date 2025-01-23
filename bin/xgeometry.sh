80be3f618f9ea485029acd1079793a543b5c09c6
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-01-23 19:31:57 +0200

    Disable unmentioned displays

--- bin/xgeometry.sh
+++ bin/xgeometry.sh
@@ -7,6 +7,7 @@ CLI=(
     --prefix "args_"
     -c "displays;Displays from left to right"
     -O "primary;Set primary display;;p"
+    -f "disable;Disable all other outputs;;d"
     -f "list;List connected outputs and quit;;l"
     -f "list-all;List all outputs and quit;;L"
 )
@@ -16,9 +17,24 @@ eval "$CLI" || exit 1
 [[ -z $args_list_all ]] || { xrandr -q | grep "connected" | sort ; exit 0 ; }
 [[ -z $args_list ]] || { xrandr -q | grep " connected" | sort ; exit 0 ; }
 
-xrandr --auto
 
 if [[ -n $args_displays ]]; then
+    # enable/disable displays
+    if [[ $args_disable ]]; then
+        # disable all displays
+        while read -r output; do
+            echo "disabling $output" >&2
+            xrandr --output "$output" --off
+        done < <(xrandr -q | grep " connected" | awk '{print $1}')
+        # enable configured displays
+        for output in "${args_displays[@]}"; do
+            echo "enabling $output" >&2
+            xrandr --output "$output" --auto
+        done
+    else
+        xrandr --auto
+    fi
+    # arrange displays
     left=${args_displays[0]}
     echo -n "$left"
     for next_display in "${args_displays[@]:1}"; do
