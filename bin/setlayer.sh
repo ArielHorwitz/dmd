a772e19fc1dff70391d874316c5d0b626ec43506
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-02-12 09:15:57 +0200

    Remove polybar from setlayer

--- bin/setlayer.sh
+++ bin/setlayer.sh
@@ -5,10 +5,8 @@ keyboard_layout=$2
 
 if [[ $layer = "base" ]]; then
     setxkbmap us
-    polybar-msg action kmd hook 0
 elif [[ $layer = "text" ]]; then
     setxkbmap $keyboard_layout
-    polybar-msg action kmd hook 1
 else
     echo "No such layer '$layer'" >&2; exit 1
 fi
