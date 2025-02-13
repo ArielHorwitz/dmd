b475bcc0ccd18c62ceae35eedd4e605f171246ea
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-02-13 13:37:46 +0200

    Rework touchpadtoggle using hyprctl

--- bin/touchpadtoggle.sh
+++ bin/touchpadtoggle.sh
@@ -12,48 +12,47 @@ CLI=(
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 eval "$CLI" || exit 1
 
-# Get touchpad device id
-DEVICE=$(xinput --list | grep -i 'touchpad' | cut -d= -f2 | cut -f1 | head -1)
+
+status_file=$XDG_RUNTIME_DIR/touchpad.status
+all_touchpads=$(hyprctl devices -j | jq -r '.mice.[].name' | grep 'touchpad')
+
+[[ -f "$status_file" ]] || printf "true" >"$status_file"
 
 get_status() {
-    ENABLED=$(xinput --list-props $DEVICE | grep -i 'Device Enabled' | cut -d: -f2 | xargs)
-    [[ $ENABLED == "1" ]] && return 0 || return 1
-}
-
-if [[ $args_status ]]; then
-    if get_status; then
-        echo "Touchpad enabled"
-        exit 0
+    if [[ $(cat "$status_file") = 'false' ]]; then
+        return 1
     else
-        echo "Touchpad disabled"
-        exit 1
+        return 0
     fi
-fi
+}
 
 # Determine toggle behavior
 if [[ -n $args_on ]]; then
-    SETAS="on"
+    setas="true"
 elif [[ -n $args_off ]]; then
-    SETAS="off"
+    setas="false"
 else
     if get_status; then
-        SETAS="off"
+        setas="false"
     else
-        SETAS="on"
+        setas="true"
     fi
+fi
+
+if [[ $setas = "true" ]]; then
+    notification_title="Enabled touchpad"
+else
+    notification_title="Disabled touchpad"
 fi
 
 notification_args=(-u low -t 1500 -h string:synchronous:touchpadtoggle)
 
-# Set property
-if [[ $SETAS == "on" ]]; then
-    xinput --set-prop "$DEVICE" "Device Enabled" 1
-    echo "Enabled touchpad"
-    [[ $args_no_notification ]] || notify-send "Enabled touchpad" "${notification_args[@]}"
-elif [[ $SETAS == "off" ]]; then
-    xinput --set-prop "$DEVICE" "Device Enabled" 0
-    echo "Disabled touchpad"
-    [[ $args_no_notification ]] || notify-send "Disabled touchpad" "${notification_args[@]}"
-else
-    exit_error "Unknown property value: $SETAS"
-fi
+# Set status
+for device in $all_touchpads; do
+    hyprctl -r -- keyword device["$device"]:enabled "$setas" >/dev/null
+done
+# Save status to file
+printf "%s" "$setas" >"$status_file"
+# Status notification
+echo "$notification_title"
+[[ $args_no_notification ]] || notify-send "$notification_title" "${notification_args[@]}"
