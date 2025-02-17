ce748e34ca2e5bf8ed8b7dcb9d998523820a28ab
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-02-17 20:24:40 +0200

    Fix screenshot for hypr

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -7,7 +7,7 @@ ABOUT="Take a screenshot"
 CLI=(
     --prefix "args_"
     -f "desktop;Capture the entire desktop, not just focused window;;d"
-    -f "prompt;Prompt for screenshot name;;p"
+    -f "prompt;Prompt for screenshot name (overrides --name);;p"
     -O "name;Screenshot name;;n"
     -O "targetdir;Target directory to save to;$HOME/temp/screen;t"
 )
@@ -18,14 +18,21 @@ eval "$CLI" || exit 1
 temp_file="$(mktemp --dry-run).png"
 timestamp=$(timestamp)
 
-scrot_args=(--file $temp_file)
-[[ $args_desktop ]] || scrot_args+=(--focused)
+grim_args=()
+if [[ -z $args_desktop ]]; then
+    region=$(hyprctl -j activewindow | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+    grim_args+=(-g "$region")
+fi
 # Take screenshot
-scrot ${scrot_args[@]}
+grim "${grim_args[@]}" "$temp_file"
 
 # Get file path and name
-[[ $args_prompt ]] && name=$(terminalprompt 'Screenshot name: ') || name=$args_name
+if [[ $args_prompt ]]; then
+    name=$(terminalprompt 'Screenshot name: ')
+else
+    name=$args_name
+fi
 target_file="${args_targetdir}/${timestamp}${name:+_$name}.png"
 # Move file
-mkdir --parents $args_targetdir
-mv $temp_file $target_file
+mkdir --parents "$args_targetdir"
+mv "$temp_file" "$target_file"
