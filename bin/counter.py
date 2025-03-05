c828ea03250990f5de8cf4a47048fb4f29beb8b7
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-03-05 08:37:13 +0200

    Add unique line counter script

---
+++ bin/counter.py
@@ -0,0 +1,20 @@
+#! /bin/python
+
+import argparse
+import collections
+import sys
+
+
+def eprint(text):
+    print(text, file=sys.stderr)
+
+
+parser = argparse.ArgumentParser("counter", description="Count unique lines.")
+args = parser.parse_args()
+
+lines = sys.stdin.read().splitlines()
+counter = collections.Counter(lines)
+print(f"       Count | Line")
+print(f"---------------------------")
+for (line, count) in counter.most_common():
+    print(f"{count:>12,} | {line}")
