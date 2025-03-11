c5e943a8b00143774637f992826fe5f6c7ab3323
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-03-11 18:14:58 +0200

    Implement dev layout script for hyprland

---
+++ bin/devlayout.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+set -e
+
+# CLI
+APP_NAME=$(basename "${0%.*}")
+ABOUT="Open all the necessary windows for a development session."
+CLI=(
+    --prefix "args_"
+    -p "dir;Project directory"
+    -f "in-home;Consider directory relative to home directory;;H"
+)
+CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
+# echo "$CLI" >&2
+eval "$CLI" || exit 1
+
+if [[ $args_in_home ]]; then
+    project_dir="$HOME/$args_dir"
+else
+    project_dir="$args_dir"
+fi
+project_name=$(basename "$project_dir")
+
+alacritty_args=(--working-directory "$project_dir")
+
+lite-xl "$project_dir" &
+alacritty "${alacritty_args[@]}" --title "$project_name" --hold --command lazygit &
+alacritty "${alacritty_args[@]}" &
+alacritty "${alacritty_args[@]}" &
