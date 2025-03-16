edd092d95d095b54f6a03c2975f8347323f809fe
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-03-16 12:24:00 +0200

    Fix relative path in devlayout

--- bin/devlayout.sh
+++ bin/devlayout.sh
@@ -6,17 +6,17 @@ APP_NAME=$(basename "${0%.*}")
 ABOUT="Open all the necessary windows for a development session."
 CLI=(
     --prefix "args_"
-    -p "dir;Project directory"
-    -f "in-home;Consider directory relative to home directory;;H"
+    -p "dir;Project directory relative to \$HOME"
+    -f "absolute;Consider directory as an absolute path"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 # echo "$CLI" >&2
 eval "$CLI" || exit 1
 
-if [[ $args_in_home ]]; then
-    project_dir="$HOME/$args_dir"
-else
+if [[ $args_absolute ]]; then
     project_dir="$args_dir"
+else
+    project_dir="$HOME/$args_dir"
 fi
 project_name=$(basename "$project_dir")
 
