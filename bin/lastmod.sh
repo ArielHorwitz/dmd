6a5be692b2809d367fa759d3d9498709e4ad8efb
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-06-12 07:55:53 +0300

    Add name sorting to lastmod

--- bin/lastmod.sh
+++ bin/lastmod.sh
@@ -8,6 +8,7 @@ CLI=(
     --prefix "args_"
     -o "dir-path;Path to directory;."
     -f "first;Show first modified instead of last;;f"
+    -f "name;Consider name instead of modified time;;n"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
 # echo "$CLI" >&2
@@ -18,4 +19,9 @@ if [[ -z $args_first ]]; then
     sort_args+=(--reverse)
 fi
 
-find "$args_dir_path" -maxdepth 1 -type f -printf '%T@ %p\n' | sort "${sort_args[@]}" | head -n1 | cut -d' ' -f2-
+file_format='%T@ %p\n'
+if [[ $args_name ]]; then
+    file_format='0 %p\n'
+fi
+
+find "$args_dir_path" -maxdepth 1 -type f -printf "$file_format" | sort "${sort_args[@]}" | head -n1 | cut -d' ' -f2-
