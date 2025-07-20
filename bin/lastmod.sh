c2bb8dda404eec1d38991621843178fbb2e34621
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2025-07-20 13:12:18 +0300

    Support filter files by pattern

--- bin/lastmod.sh
+++ bin/lastmod.sh
@@ -7,7 +7,8 @@ ABOUT="Print the last modified entry of a directory"
 CLI=(
     --prefix "args_"
     -o "dir-path;Path to directory;."
-    -f "first;Show first modified instead of last;;f"
+    -O "filter;Filter files by pattern (shell glob);*;f"
+    -f "first;Show first modified instead of last;;F"
     -f "name;Consider name instead of modified time;;n"
 )
 CLI=$(spongecrab --name "$APP_NAME" --about "$ABOUT" "${CLI[@]}" -- "$@") || exit 1
@@ -24,4 +25,4 @@ if [[ $args_name ]]; then
     file_format='0 %p\n'
 fi
 
-find "$args_dir_path" -maxdepth 1 -type f -printf "$file_format" | sort "${sort_args[@]}" | head -n1 | cut -d' ' -f2-
+find "$args_dir_path" -maxdepth 1 -type f -name "$args_filter" -printf "$file_format" | sort "${sort_args[@]}" | head -n1 | cut -d' ' -f2-
