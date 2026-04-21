f0a8773441a7f12541e1943ac72b58a4ec1bc7c8
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2026-04-21 09:12:31 +0300

    Use fuzzel launcher

---
+++ bin/run-menu.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -e
+
+HIST=~/.cache/run-menu-history
+mkdir -p "$(dirname "$HIST")"
+touch "$HIST"
+
+cmd=$(fuzzel --dmenu --prompt '$ ' < "$HIST")
+[[ -n $cmd ]] || exit 0
+
+{ printf '%s\n' "$cmd"; grep -vxF -- "$cmd" "$HIST" || :; } > "$HIST.tmp"
+mv "$HIST.tmp" "$HIST"
+
+setsid -f sh -c "$cmd"
