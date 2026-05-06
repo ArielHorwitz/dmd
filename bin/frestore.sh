5bc1403252d3cd4c2dd9fd76e0c7744c29fd0339
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2026-05-07 00:17:42 +0300

    Restore lockscreen

--- bin/frestore.sh
+++ bin/frestore.sh
@@ -7,3 +7,8 @@ killall kmonad
 # Restore monitors
 hyprctl --instance 0 keyword monitor ",preferred,auto,auto"
 hyprctl --instance 0 keyword monitor "eDP-1,preferred,auto,auto"
+
+# Restore lockscreen
+hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1'
+sleep 0.2
+hyprctl --instance 0 'dispatch exec hyprlock'
