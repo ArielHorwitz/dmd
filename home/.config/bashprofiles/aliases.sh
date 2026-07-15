f23b3eb308e62585e7b4b098494a424e46122156
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2026-07-15 09:56:19 +0300

    Rename casebook cli tool

--- home/.config/bashprofiles/aliases.sh
+++ home/.config/bashprofiles/aliases.sh
@@ -21,7 +21,7 @@ alias egrep='egrep --colour=auto'
 alias fgrep='fgrep --colour=auto'
 alias printenv='printenv -0 | sort -z | tr "\0" "\n"'
 
-alias cb="casebook"
+alias cb="casebook-cli"
 
 alias lastcmd="HISTTIMEFORMAT='' history 2 | head -n1 | awk '{\$1=\"\"; print substr(\$0, 2)}'"
 alias lastcmdcopy="lastcmd | clipcatctl load"
