3ce2e51f5819cf9448226ba5bdf8e93c17249007
Ariel Horwitz <ArielHorwitz@users.noreply.github.com>
2026-08-04 09:19:05 +0300

    Add border animation

--- home/.config/hypr/hyprland/look.lua
+++ home/.config/hypr/hyprland/look.lua
@@ -81,7 +81,7 @@ hl.curve("quick", { type = "bezier", points = { { 0.15, 0 }, { 0.1, 1 } } })
 
 hl.animation({ leaf = "global", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "border", enabled = true, speed = 5.39, bezier = "easeOutQuint" })
--- hl.animation({ leaf = "borderangle", enabled = true, speed = 10, bezier = "easeOutCubic", style = "loop" })
+hl.animation({ leaf = "borderangle", enabled = true, speed = 30, bezier = "linear", style = "loop" })
 hl.animation({ leaf = "windows", enabled = true, speed = 4.79, bezier = "easeOutQuint" })
 hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.1, bezier = "easeOutQuint", style = "popin 87%" })
 hl.animation({ leaf = "windowsOut", enabled = true, speed = 1.49, bezier = "linear", style = "popin 87%" })
